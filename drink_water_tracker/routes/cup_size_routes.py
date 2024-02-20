ce719720d22289fbb46eb782a0a8aa9c1038d672
Carlos Chiarelli <ca.chiarelli.97@gmail.com>
2024-02-20 13:29:42 +0000

    feat: implement 'add cup size' feature
    Adds the 'add cup size' feature by implementing and testing the use case and route.
    
    Closes JIRA-4.1

--- drink_water_tracker/routes/cup_size_routes.py
+++ drink_water_tracker/routes/cup_size_routes.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
 
 from drink_water_tracker.routes.deps import get_db_session
+from drink_water_tracker.schemas.cup_size import CupSize
 from drink_water_tracker.use_cases.cup_size import CupSizeUseCases
 
 router = APIRouter(prefix="/cup-size", tags=["cup-size"])
+
+
+@router.post("/add")
+def add_cup_size(cup_size: CupSize, db_session: Session = Depends(get_db_session)):
+    uc = CupSizeUseCases(db_session=db_session)
+    uc.add_cup_size(cup_size=cup_size)
+    return Response(status_code=status.HTTP_201_CREATED)
 
 
 @router.get("/list")
--- tests/routes/test_cup_size_routes.py
+++ tests/routes/test_cup_size_routes.py
@@ -1,9 +1,22 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from drink_water_tracker.db.models import CupSize as CupSizeModel
 from drink_water_tracker.main import app
 
 client = TestClient(app)
+
+
+def test_add_cup_size_route(db_session):
+    body = {"description": "Copo médio 350mL", "amount_ml": 350}
+    response = client.post("/cup-size/add", json=body)
+    assert response.status_code == status.HTTP_201_CREATED
+
+    cup_size_on_db = db_session.query(CupSizeModel).all()
+    assert len(cup_size_on_db) == 1
+
+    db_session.delete(cup_size_on_db[0])
+    db_session.commit()
 
 
 def test_list_cup_sizes_route(cup_sizes_on_db):
